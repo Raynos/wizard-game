f40cc172b72cc1ffcc7c4a93a6aa62bd45662847
James Halliday <mail@substack.net>
2012-11-11 12:46:20 -0800

    live code edits work

--- browser/ui/editor.js
+++ browser/ui/editor.js
@@ -1,4 +1,6 @@
 var wrap = require('../../wrap')
+var ever = require('ever')
+var deepEqual = require('deep-equal')
 
 module.exports = Editor
 
@@ -27,7 +29,7 @@ function Code(world) {
         tj.style.display = 'block'
     }))
     tabs.querySelector('.tab').className = 'tab active'
- 
+
     function createTab (txt, cb) {
         var div = document.createElement('div')
         div.className = 'tab'
@@ -37,6 +39,7 @@ function Code(world) {
             if (o) o.className = 'tab'
             div.className = 'tab active'
             cb.call(div, ev)
+            ever({ codex : ta, tome : tj }[txt]).emit('change')
         })
         
         return div
@@ -50,31 +53,70 @@ function Code(world) {
         div.addEventListener('click', function () {
             var name = document.querySelector('.tab.active').textContent
             console.log('cast ' + name)
+
+            if (name === 'codex') {
+                current.set('source', ta.value)
+            }
+            else if (name === 'tome') {
+                var next
+                try { next = JSON.parse(tj.value) }
+                catch (err) { tj.className = 'json error'; return }
+ 
+                tj.className = 'json'
+
+                var ch = Object.keys(current.state).reduce(function (acc, key) {
+                    if (!deepEqual(current.state[key], next[key])) {
+                        acc[key] = next[key]
+                    }
+                    return acc
+                })
+                current.set(ch)
+            }
         })
+ 
+        var current
+        world.on('examine', function (row) { current = row })
+
+        function onchange () {
+            if (!current) return
+
+            var name = document.querySelector('.tab.active').textContent
+            if (name === 'codex') {
+                try {
+                    Function(ta.value)
+                }
+                catch (err) {
+                    ta.className = 'code error'
+                    div.className = 'cast tab disable'
+ 
+                    return
+                }
+                ta.className = 'code'
+                div.className = 'cast tab'
+            }
+            else if (name === 'tome') {
+                try {
+                    JSON.parse(tj.value)
+                }
+                catch (err) {
+                    tj.className = 'json error'
+                    return
+                }
+                tj.className = 'json'
+            }
+        }
+
+        tj.addEventListener('change', onchange)
+        tj.addEventListener('keydown', onchange)
+
+        ta.addEventListener('change', onchange)
+        ta.addEventListener('keydown', onchange)
+        
         return div
     })())
 
     document.body.appendChild(tabs)
 
-    var _row
-    function onChange () {
-        if(!_row) return
-        var fun
-        try {
-          fun = wrap(ta.value) //try and parse this...
-        } catch (err) {
-          return world.emit('log', err.toString())
-        }
-        //EXECUTE THE CODE
-        world.emit('log', 'update: '+_row.id)
-        _row.set('source', ta.value)
-//        if(_row) fun(_row.api)
-    }
-    var timer
-    ta.oninput = function () {
-      clearTimeout(timer)
-      timer = setTimeout(onChange, 500)
-    }
     world.on("examine", function (row) {
       world.emit('log', 'examine: '+ (row.id || row))
         if(!row.get || !row.get('source')) return
