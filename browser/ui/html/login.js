4e06d4320d497a3c35be97c2439358fa227c3152
James Halliday <mail@substack.net>
2012-11-11 10:02:29 -0800

    vendor ace

--- browser/ui/html/login.js
+++ browser/ui/html/login.js
@@ -1,7 +1,12 @@
-module.exports = '<div class="top">' +
+module.exports = '<div class="top">{' +
+    '"<span class="key">name</span>" :' +
     '<div id="login" class="login">' +
         '<input id="field"/>' +
         '<button id="button">set name</button>' +
     '</div>' +
-    '<div id="colors" class="color-picker"></div>' +
-'</div>'
+    '<span id="name" class="value"></span>, ' +
+    '"<span class="key">color</span>" : ' +
+    '<div id="colors" class="color-picker value"></div>' +
+    ', "<span class="key">beep</span>" : ' +
+    '<span class="value">boop</span>' +
+' }</div>'
