24a8dfbabbaab4db927566f088ed693deb9c332d
James Halliday <mail@substack.net>
2012-11-10 23:49:32 -0800

    monsters

--- browser/entities/monster.js
+++ browser/entities/monster.js
@@ -1,11 +1,18 @@
-var Entity = require("./entity")
+var Sprite = require("./sprite")
 
 module.exports = monster
 
-function monster(paper, relative) {
-    var entity = Entity(paper, relative)
+function monster(paper, relative, row) {
+    var rs = [
+        { file : '/monster_0.svg', width: 150, height : 115 }
+    ]
+    var r = rs[Math.floor(Math.random() * rs.length)]
 
-    entity.attr("fill", "brown")
+    var entity = Sprite(paper, relative, {
+        files : { monster : [ r ] }
+        , computeKey : function () { return 'monster' }
+        , row : row
+    })
 
     return entity
 }
