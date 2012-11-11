a9ba7b03ad8a968769b1e29eaddb67ef3b4971c5
Dominic Tarr <dominic.tarr@gmail.com>
2012-11-11 11:20:05 -0800

    shorter id

--- model.js
+++ model.js
@@ -2,14 +2,14 @@ var crdt = require('crdt')
 var model = module.exports = new (crdt.Doc)
 
 var ids = 1
-function uuid () {
-  return ids++
+function id (type) {
+  return type+'_'+ids++
 }
 
 model.create = function (type) {
     console.log('create', type)
     var row =  model.add({
-          id: type + '_' + uuid()
+          id: id(type)
         , x: Math.round(Math.random() * 600)
         , y: Math.round(Math.random() * 480)
         , type: type
