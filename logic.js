fac3ed2a8191c1b676b19ac69d5b5153ab482bae
Dominic Tarr <dominic.tarr@gmail.com>
2012-11-11 11:00:23 -0800

    catch errors

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
-
 var model = require('./model')
-
 var wrap = require('./wrap')
+
+module.exports = api
 
 function isFunction (f) {
   return 'function' === typeof f
@@ -71,9 +71,19 @@ function api (row) {
     var _listener
     return function (listener) {
       model.removeListener(event, listener)
-      if(isFunction(listener))
-        model.on(event, _listener = wrapper(listener))
-            
+      if(isFunction(listener)) {
+        var wrapped = wrapper(listener)
+        model.on(event, _listener = function () {
+          try {
+            wrapped.apply(this, arguments)
+          } catch (err) {
+            //TODO: present this back to the user somehow...
+            //or, make the entity die.
+            row.message({text: err.toString().toUpperCase(), stroke: 'red', fill: 'black'})
+            console.error('user-error', err.toString().toUpperCase())
+          }
+        })
+      }
       return self
     }
   }
@@ -200,72 +210,5 @@ function api (row) {
   }
 
   return self
-}
-
-model.on('create', function (row) {
-  //on the first update, set api stuff...
-  row.api = api(row)
-  row.once('update', function () {
-    // console.log('create', row.toJSON())
-      // console.log(row)
-    var _fn
-    row.on('change', function (ch) {
-      var fn
-      if(ch.source) {
-        try {
-          fn = wrap(ch.source)(row.api)
-        } catch (err) {
-          console.error(err, ch.source)
-        }
-      }
-    })
-
-    if(row.get('type') === 'monster') {
-      row.set("source", string(init))
-    } else if (row.get("type") === "tree") {
-      row.set("source", string(tree))
-    } else if (row.get("type") === "rock") {
-      row.set("source", string(rock))
-    }
-  })
-})
-
-function tree() {
-  // I am a tree
-}
-
-function rock() {
-  // I am a rock
-}
-
-//this function is eval'd (the user will enter it as text...)
-/*global self*/
-
-//remove indentation, so that it displays property in the text editor
-function init () {
-self.say('hello')
-self.think(function () {
-  function r () {
-    return (Math.random()*2 - 1)
-  }
-
-  var x = r(), y = r()
-  var l = Math.sqrt(x*x + y*y)
-  x = x / l; y = y / l
-  self.move(x*10, y*10)
-
-  if(Math.random() < 0.1)
-    self.say('woof')
-})
-
-//if another monster speaks nearby...
-self.hear(function (words, id) {
-  //...
-})
-}
-
-function string(code) {
-  code = code.toString()
-  return code.substring(code.indexOf('{') + 1, code.length - 2)
 }
 
